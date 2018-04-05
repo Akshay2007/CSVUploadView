75e13616f7e96290f12113d871a251792e26534a
Akshay2007 <31475284+Akshay2007@users.noreply.github.com>
2018-04-05 20:09:13 +0530

    Add files via upload

--- viewport.js
+++ viewport.js
@@ -1,51 +1,65 @@
 var dataModel = Ext.define('info', {
-	extend: 'Ext.data.Model',
-    fields:[
-		{name: 'name', type: 'string'},
-		{name: 'email', type: 'string'},
-		{name: 'phone', type: 'string'},
-		{name: 'address', type: 'string'},
-		{name: 'insurType', type: 'string'}
-    ]
+  extend: 'Ext.data.Model',
+  fields: [{
+      name: 'name',
+      type: 'string'
+    },
+    {
+      name: 'email',
+      type: 'string'
+    },
+    {
+      name: 'phone',
+      type: 'string'
+    },
+    {
+      name: 'address',
+      type: 'string'
+    },
+    {
+      name: 'insurType',
+      type: 'string'
+    }
+  ]
 });
 
 
-var datastore = Ext.create('Ext.data.Store', {	
-	storeId:'PatientsStore',
-	model:dataModel,
-	proxy : {
-		type : 'rest',
-		url : '/readCSV',
-		method : "GET",
-		useDefaultXhrHeader : false,
-		headers : {
-			'Content-Type' : 'application/json',
-			'Access-Control-Allow-Origin' : '*'
-		},
-		noCache : false,
+var datastore = Ext.create('Ext.data.Store', {
+  storeId: 'PatientsStore',
+  model: dataModel,
+  proxy: {
+    type: 'rest',
+    url: '/readCSV',
+    method: "GET",
+    useDefaultXhrHeader: false,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    noCache: false,
 
-		reader : {
-			type : 'json',
-		}
-	},
-     	autoLoad: true  
-    
+    reader: {
+      type: 'json',
+    }
+  },
+  autoLoad: true
+
 });
 
 Ext.onReady(function() {
   Ext.Loader.setConfig({
     enabled: true
   });
-  Ext.create('Ext.tab.Panel', {	
+  Ext.create('Ext.tab.Panel', {
     renderTo: Ext.getBody(),
     fullscreen: true,
     items: [{
         xtype: 'panel',
-        title: 'Upload File',
+        title: 'Upload Patients Details',
 
         items: [{
           xtype: 'form',
-          itemId: 'uploadForm', 
+          itemId: 'uploadForm',
           height: 200,
           margin: '75 50 50 570',
           top: 100,
@@ -61,56 +75,53 @@ Ext.onReady(function() {
               regex: /^.*\.(csv|CSV)$/,
               regexText: 'Only CSV files allowed'
             },
-            
+
             {
               xtype: 'button',
               text: 'Upload',
               width: 70,
-              margin: '0 0 0 20',
               handler: function() {
                 var form = Ext.ComponentQuery.query('#uploadForm');
 
-                if (form[0].form.isValid()) 
-                {	form[0].getForm().submit({
-                  url: '/uploadCSV',
-                /*  waitMsg: 'Uploading Please Wait...',*/
-                  method: 'POST',
-                  success: function(r, a) {
-                    console.log('service call success');
-                    	Ext.getCmp('grid').getStore().load();
-                    				Ext.getCmp('grid').getView().refresh();	 
+                if (form[0].form.isValid()) {
+                  form[0].getForm().submit({
+                    url: '/uploadCSV',
+                    /*  waitMsg: 'Uploading Please Wait...',*/
+                    method: 'POST',
+                    success: function(r, a) {
+                      console.log('service call success');
+                      Ext.getCmp('grid').getStore().load();
+                      Ext.getCmp('grid').getView().refresh();
 
-                  },
-                  failure: function(r, a) {
-                    console.log('service call fail');
-                  }
-                });
+                    },
+                    failure: function(r, a) {
+                      console.log('service call fail');
+                    }
+                  });
 
 
-                var datastore = Ext.create('Ext.data.Store', {	
-                	storeId:'PatientsStore',
-                	model:dataModel,
-                	proxy : {
-                		type : 'rest',
-                		url : '/readCSV',
-                		method : "GET",
-                		useDefaultXhrHeader : false,
-                		headers : {
-                			'Content-Type' : 'application/json',
-                			'Access-Control-Allow-Origin' : '*'
-                		},
-                		noCache : false,
+                  var datastore = Ext.create('Ext.data.Store', {
+                    storeId: 'PatientsStore',
+                    model: dataModel,
+                    proxy: {
+                      type: 'rest',
+                      url: '/readCSV',
+                      method: "GET",
+                      useDefaultXhrHeader: false,
+                      headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                      },
+                      noCache: false,
 
-                		reader : {
-                			type : 'json',
-                		}
-                	},
-                     	autoLoad: true  
-                    
-                });
-                } 
+                      reader: {
+                        type: 'json',
+                      }
+                    },
+                    autoLoad: true
 
-                else {
+                  });
+                } else {
                   alert("Enter a CSV file");
                   form[0].form.reset();
                 }
@@ -118,47 +129,75 @@ Ext.onReady(function() {
               scope: this,
               title: 'save',
               overflowText: 'horizontal ruler'
-            }]
+            }
+          ]
         }]
       },
       {
         title: 'Patients Details',
         xtype: 'container',
-	layout: {type: 'hbox'},
-    	width: 400,
-    	
-    	border: 1,
-    	style: {borderColor:'#000000', borderStyle:'solid', borderWidth:'1px'},
-    	defaults: {
-        	labelWidth: 80,
-        	flex: 1,
-        	style: {padding: '10px' }
-    	},
-	items:[{
-		xtype:'panel',
-		items:[{
-		xtype:'grid',
-		id:"grid",
-		title: 'Patients Details',
-		store: datastore,
-		columns: [
-			{ header: 'Name',  dataIndex: 'name',flex: 0.25 },
-			{ header: 'Email', dataIndex: 'email', flex: 0.5 },
-			{ header: 'Phone', dataIndex: 'phone' },
-			{ header: 'Address', dataIndex: 'address', flex: 0.5 },
-			{ header: 'Insurance Type', dataIndex: 'insurType'}
-		],
-		height: 200,
-		},
-		{
-	    	xtype: 'button',
-	      text: 'refresh',
-	      handler: function() {
-	      				Ext.getCmp('grid').getStore().load();
-				Ext.getCmp('grid').getView().refresh();	
-	      }
-	    }],
-		}]
+        layout: {
+          type: 'hbox'
+        },
+        width: 400,
+
+        border: 1,
+        style: {
+          borderColor: '#000000',
+          borderStyle: 'solid',
+          borderWidth: '1px'
+        },
+        defaults: {
+          labelWidth: 80,
+          flex: 1,
+          style: {
+            padding: '10px'
+          }
+        },
+        items: [{
+          xtype: 'panel',
+          autoScroll: true,
+          items: [{
+              xtype: 'button',
+              text: 'Refresh',
+              handler: function() {
+                Ext.getCmp('grid').getStore().load();
+                Ext.getCmp('grid').getView().refresh();
+              }
+            },
+            {
+              xtype: 'grid',
+              id: "grid",
+              title: 'Patients Details',
+              store: datastore,
+              columns: [{
+                  header: 'Name',
+                  dataIndex: 'name',
+                  flex: 0.25
+                },
+                {
+                  header: 'Email',
+                  dataIndex: 'email',
+                  flex: 0.5
+                },
+                {
+                  header: 'Phone',
+                  dataIndex: 'phone'
+                },
+                {
+                  header: 'Address',
+                  dataIndex: 'address',
+                  flex: 0.5
+                },
+                {
+                  header: 'Insurance Type',
+                  dataIndex: 'insurType'
+                }
+              ],
+            height: 200
+            }
+          ],
+        }]
 
       }
     ],
